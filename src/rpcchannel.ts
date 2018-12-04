bb04e01d6bbe192798ee6c9e077643c4409cd07e
Joël Charles <joel.charles91@gmail.com>
2018-12-04 18:39:35 +0100

    Some events were lost in the outerspace

--- src/rpcchannel.ts
+++ src/rpcchannel.ts
@@ -1,4 +1,5 @@
-import { EventEmitter } from 'events';
+import { fromEvent } from 'rxjs';
+import { filter, first, map, shareReplay } from 'rxjs/operators';
 import { Duplex } from 'stream';
 import RPCPeer from './peer';
 import { RPCChannel, RPCChannelOptions, RPCChannelPeer } from './types';
@@ -7,20 +8,28 @@ const { BPMux } = require('bpmux');
 
 export default function rpcchannel(duplex: Duplex, options: RPCChannelOptions = {}): RPCChannel {
   const mux = new BPMux(duplex);
-  const em = new EventEmitter();
+  const handshakeObservable = fromEvent(mux, 'handshake')
+    .pipe(
+      map(([d, data, pause]: [Duplex, Buffer, Function | null]) => {
+        const linkId = data.toString();
+        if (pause) pause();
+        return [linkId, d];
+      }),
+      shareReplay(10, 2000),
+    );
 
-  mux.on('handshake', (d: Duplex, data: Buffer, pause: Function | null) => {
-    const linkId = data.toString();
-    if (pause) pause();
-    em.emit(`handshake:${linkId}`, d);
-  });
+  handshakeObservable.subscribe();
 
   return {
     peer(linkId: string): RPCChannelPeer {
       const peer = new RPCPeer(options.defaultRequestTimeout);
-      em.once(`handshake:${linkId}`, (d: Duplex) => {
-        d.pipe(peer);
-      });
+      handshakeObservable
+        .pipe(
+          filter(([id]) => linkId === id),
+          first(),
+        ).subscribe(([, d]: [string, Duplex]) => {
+          d.pipe(peer);
+        });
       const peerWrapper = mux.multiplex({
         handshake_data: Buffer.from(linkId),
       });
