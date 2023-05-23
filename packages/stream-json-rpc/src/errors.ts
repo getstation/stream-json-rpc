0abb951f59b2709bf869acc27d90f1003295a9f3
Joël Charles <joel.charles91@gmail.com>
2023-05-23 12:35:23 +0200

    simplify wrapError

--- packages/stream-json-rpc/src/errors.ts
+++ packages/stream-json-rpc/src/errors.ts
@@ -1,7 +1,5 @@
-import { JsonRpcError } from 'json-rpc-protocol';
-
-export const wrapError = (e: JsonRpcError) => {
-  if (e.data && (e.data as any).stack) {
+export const wrapError = (e: any) => {
+  if (e.data && e.stack && e.data.stack) {
     e.stack = `${e.stack}
 Caused by: ${(e.data as any).stack}`;
     delete (e.data as any).stack;
